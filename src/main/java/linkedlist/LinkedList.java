8163e718e3d352d075cb945016a44eb1b15bf9a0
PilotChalkanov <nik.chalkanov@gmail.com>
2024-02-16 10:19:11 +0200

    Add LinkedList

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -1,4 +1,5 @@
 package linkedlist;
+
 public class LinkedList {
     private Node head;
     private Node tail;
@@ -12,44 +13,101 @@ public class LinkedList {
 
     }
 
-    public void printList() {
-        StringBuilder result = new StringBuilder();
-        Node temp = head;
-        result.append(head.value);
-        while (temp.next != null) {
-            result.append(" -> " + temp.next.value);
-            temp = temp.next;
-        }
-        System.out.println(result);
+    public LinkedList() {
+        head = null;
+        tail = null;
+        length = 0;
     }
 
-    public void append(int value){
+    public void printList() {
+        StringBuilder result = new StringBuilder();
+        if (length > 0) {
+            Node temp = head;
+            result.append(head.value);
+            while (temp.next != null) {
+                result.append(" -> " + temp.next.value);
+                temp = temp.next;
+            }
+        }
+        System.out.println(result);
+
+    }
+
+    public void append(int value) {
         Node newNode = new Node(value);
         if (length != 0) {
             tail.next = newNode;
             tail = newNode;
-        }
-        else {
+        } else {
             head = newNode;
             tail = newNode;
         }
         length += 1;
     }
 
-    public void getHead(){
-        System.out.println(head);
-    }
-    public void getTail(){
-        System.out.println(tail);
+    public void prepend(Integer value) {
+        Node temp = head;
+        head = new Node(value);
+        head.next = temp;
+        length++;
     }
 
-    class Node {
-        int value;
-        Node next;
-
-        Node(int v) {
-            this.value = v;
-            this.next = null;
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        int i = 0;
+        Node currentNode = head;
+        while (i++ < index) {
+            currentNode = currentNode.next;
         }
+        return currentNode;
+
     }
+
+    public boolean set(int index, int value) {
+       Node temp = get(index);
+       if (temp != null){
+           temp.value = value;
+           return true;
+       }
+       return false;
+    }
+
+    public Integer popLeft() {
+        if (head != null) {
+            Node left = head;
+            head = head.next;
+            length--;
+            if (length == 0) tail = null;
+            return left.value;
+        }
+        return null;
+    }
+
+    public Integer pop() {
+        Node temp = head;
+        Node pre = head;
+        if (length == 0) return null;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp.value;
+    }
+
+    public Integer getHead() {
+        return (head != null) ? head.value : null;
+    }
+
+    public Integer getTail() {
+        return (tail != null) ? tail.value : null;
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,32 @@ package org.example;
 import hashtable.HashTable;
 import linkedlist.LinkedList;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
 
-        HashTable myHashTable = new HashTable();
-        myHashTable.set("nails", 10);
-        myHashTable.set("nails", 120);
-        myHashTable.set("fax", 100);
-        myHashTable.set("brother", 5);
-        myHashTable.set("gutter", 7);
-        myHashTable.set("albano", 1);
-        System.out.println(myHashTable.get("nails"));
-        System.out.println(myHashTable.get("gutter"));
-        System.out.println(myHashTable.get("brother"));
 
-        LinkedList myLinkedList = new LinkedList(3);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
         myLinkedList.append(4);
-        myLinkedList.append(4);
-        myLinkedList.append(4);
+        System.out.println(myLinkedList.set(0, -2));
         myLinkedList.printList();
+//        System.out.println(myLinkedList.get(0).value);
+        System.out.println(myLinkedList.get(1).value);
+//        System.out.println(myLinkedList.popLeft());
+//        System.out.println(myLinkedList.popLeft());
+//        System.out.println(myLinkedList.popLeft());
+//        System.out.println(myLinkedList.popLeft());
+//        System.out.println(myLinkedList.popLeft());
+        myLinkedList.printList();
+
+//        LinkedList myNewList = new LinkedList();
+//        myNewList.prepend(1);
+//        myNewList.printList();
+//        Integer item = myNewList.popLeft();
+//        System.out.println(item);
 
     }
 }
